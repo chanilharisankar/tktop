6a5884bc248865b78a5c08f157c62f1e789afacd
K Hari Sankar <chanil.harisankar@gmail.com>
2026-06-17 12:58:37 +0530

    chore(release): prepare v1.1.2

--- src/tktop/__init__.py
+++ src/tktop/__init__.py
@@ -3,4 +3,4 @@ from importlib.metadata import PackageNotFoundError, version
 try:
     __version__ = version("tktop")
 except PackageNotFoundError:
-    __version__ = "1.1.1"
+    __version__ = "1.1.2"
